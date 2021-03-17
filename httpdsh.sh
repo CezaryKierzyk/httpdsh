bf42cf673a4e6b395990f6e9d20acb1308957c94
Cezary Kierzyk <cezary.97@interia.eu>
2021-03-17 19:04:51 +0100

    added flag parsing for the server

--- httpdsh.sh
+++ httpdsh.sh
@@ -5,14 +5,35 @@ declare -a PWD="${HOME}/.httpdsh/files/"
 declare -a PORT="8080"
 
 ###################_____FUNCTIONS_____###################
+function print_help (){
+	echo "	Simple POSIX Shell HTTP server
+	USAGE:
+	./httpdsh [option] <parameter>
 
-function check_directory(){
-	if [ -z $(ls -a ~ | grep ".httpdsh") ]
-	then
-		mkdir -p $PWD
-	fi	
+	OPTIONS:
+
+	-p <port_number> - choose port number to listen on
+	-d <directory path> - choose working directory of the server
+	-h display this help
+	"
+	exit
 }
+function check_directory(){ ! [ -d "$PWD" ] && mkdir -p "$PWD"; }
 
 ###################________MAIN__________##################
+while test $# -gt 0
+do
+	case $1 in
+		'-p') PORT=$2
+			;;
+		'-d') PWD="$2"
+			;;
+		'-*') print_help
+			;;
+		'-h') print_help
+			;;
+	esac
+	shift
+done
 check_directory
 ncat -k -lp $PORT -e "./handler.sh $PWD" 
